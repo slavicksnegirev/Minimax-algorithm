96325ca31409946e81e7d3294d235be2a9bc5441
Slavick <92212345+slavicksnegirev@users.noreply.github.com>
2023-04-27 13:25:50 +0300

    add vizualization of tree

--- main.py
+++ main.py
@@ -1,126 +1,112 @@
-import sys
-import numpy as np
 import networkx as nx
-from matplotlib import pyplot as plt
-
-path = []
-text_output = []
-G = nx.Graph()
-
-G.add_nodes_from([
-    ("1", {"heuristics": 14}),
-    ("2", {"heuristics": 13}),
-    ("3", {"heuristics": 12}),
-    ("4", {"heuristics": 11}),
-    ("5", {"heuristics": 10}),
-    ("6", {"heuristics": 9}),
-    ("7", {"heuristics": 8}),
-    ("8", {"heuristics": 7}),
-    ("9", {"heuristics": 6}),
-    ("10", {"heuristics": 5}),
-    ("11", {"heuristics": 4}),
-    ("12", {"heuristics": 3}),
-    ("13", {"heuristics": 2}),
-    ("14", {"heuristics": 1}),
-    ("15", {"heuristics": 0}),
-    ("16", {"heuristics": 14}),
-    ("17", {"heuristics": 13}),
-    ("18", {"heuristics": 12}),
-    ("19", {"heuristics": 11}),
-    ("20", {"heuristics": 10}),
-    ("21", {"heuristics": 9}),
-    ("22", {"heuristics": 8}),
-    ("23", {"heuristics": 7}),
-    ("24", {"heuristics": 6}),
-    ("25", {"heuristics": 5}),
-    ("26", {"heuristics": 4}),
-    ("27", {"heuristics": 3}),
-    ("28", {"heuristics": 2}),
-    ("29", {"heuristics": 1}),
-    ("30", {"heuristics": 0}),
-])
-
-G.add_edges_from([
-    ("1", "2", {"weight": 0}),
-    ("1", "3", {"weight": 0}),
-
-])
-# G.add_edges_from([
-#     ("1", "2", {"weight": 10}),
-#     ("1", "3", {"weight": 12}),
-#     ("1", "5", {"weight": 23}),
-#     ("2", "4", {"weight": 14}),
-#     ("2", "5", {"weight": 6}),
-#     ("2", "6", {"weight": 18}),
-#     ("3", "4", {"weight": 4}),
-#     ("3", "5", {"weight": 20}),
-#     ("3", "7", {"weight": 34}),
-#     ("4", "6", {"weight": 26}),
-#     ("4", "7", {"weight": 7}),
-#     ("4", "9", {"weight": 40}),
-#     ("5", "6", {"weight": 7}),
-#     ("5", "7", {"weight": 10}),
-#     ("5", "8", {"weight": 23}),
-#     ("6", "8", {"weight": 3}),
-#     ("6", "12", {"weight": 14}),
-#     ("7", "10", {"weight": 6}),
-#     ("8", "10", {"weight": 1}),
-#     ("8", "12", {"weight": 8}),
-#     ("9", "10", {"weight": 5}),
-#     ("9", "13", {"weight": 8}),
-#     ("10", "11", {"weight": 1}),
-#     ("10", "13", {"weight": 4}),
-#     ("11", "12", {"weight": 4}),
-#     ("11", "15", {"weight": 9}),
-#     ("12", "14", {"weight": 6}),
-#     ("12", "15", {"weight": 4}),
-#     ("13", "14", {"weight": 3}),
-#     ("14", "15", {"weight": 6}),
-# ])
+from networkx.drawing.nx_agraph import write_dot, graphviz_layout
+import matplotlib.pyplot as plt
+# G = nx.DiGraph()
+#
+# G.add_node("ROOT")
+#
+# for i in range(5):
+#     G.add_node("Child_%i" % i)
+#     G.add_node("Grandchild_%i" % i)
+#     G.add_node("Greatgrandchild_%i" % i)
+#
+#     G.add_edge("ROOT", "Child_%i" % i)
+#     G.add_edge("Child_%i" % i, "Grandchild_%i" % i)
+#     G.add_edge("Grandchild_%i" % i, "Greatgrandchild_%i" % i)
+#
+# # write dot file to use with graphviz
+# # run "dot -Tpng test.dot >test.png"
+# write_dot(G,'test.dot')
+#
+# # same layout using matplotlib with no labels
+# # plt.title('draw_networkx')
+# pos =graphviz_layout(G, prog='dot')
+# nx.draw(G, pos, with_labels=False, arrows=True)
+# plt.show()
 
 
 
-def draw_path():
-    tmp_paar_path = []
-    for i in range(len(path) - 1):
-        tmp_paar_path.append(path[i])
-        tmp_paar_path.append(path[i + 1])
-    tmp_paar_path = np.array(tmp_paar_path).reshape(-1, 2)
-    # print(tmp_paar_path)
 
-    tmp_edge_array = []
-    for u, v, d in G.edges:
-        tmp_edge_array.append(u)
-        tmp_edge_array.append(v)
-    tmp_edge_array = np.array(tmp_edge_array).reshape(-1, 2)
-    # print(tmp_edge_array)
+# import networkx as nx
+# import matplotlib.pyplot as plt
+# from networkx.drawing.nx_agraph import graphviz_layout
+#
+#
+# def main():
+#     # Create a directed graph
+#     G = nx.DiGraph()
+#
+#     # An example
+#     l = [('a', 'b'),
+#          ('b', 'c'),
+#          ('c', 'd'),
+#          ('d', 'e'),
+#          ('e', 'f'),
+#          ('w', 'x'),
+#          ('w', 't'),
+#          ('t', 'q'),
+#          ('q', 'r'),
+#          ('q', 'u')]
+#
+#     # Build up a graph
+#     for t in l:
+#         G.add_edge(t[0], t[1])
+#
+#     # Plot trees
+#     pos = graphviz_layout(G, prog='dot')
+#     nx.draw(G, pos, with_labels=True, arrows=False)
+#
+#     plt.savefig('draw_trees_with_pygraphviz.png', bbox_inches='tight')
+#     plt.show()
+#
+#
+# if __name__ == '__main__':
+#     main()
 
-    if len(path) > 0:
-        edge_colors = []
-        for u1, v1 in tmp_edge_array:
-            flag = 0
-            for u2, v2 in tmp_paar_path:
-                if u1 == u2 and v1 == v2:
-                    flag = 1
-                else:
-                    pass
-            edge_colors.append(flag)
-        return np.array(edge_colors)
+
+def hierarchy_pos(G, root, levels=None, width=1., height=1.):
+    '''If there is a cycle that is reachable from root, then this will see infinite recursion.
+       G: the graph
+       root: the root node
+       levels: a dictionary
+               key: level number (starting from 0)
+               value: number of nodes in this level
+       width: horizontal space allocated for drawing
+       height: vertical space allocated for drawing'''
+    TOTAL = "total"
+    CURRENT = "current"
+    def make_levels(levels, node=root, currentLevel=0, parent=None):
+        """Compute the number of nodes for each level
+        """
+        if not currentLevel in levels:
+            levels[currentLevel] = {TOTAL : 0, CURRENT : 0}
+        levels[currentLevel][TOTAL] += 1
+        neighbors = G.neighbors(node)
+        for neighbor in neighbors:
+            if not neighbor == parent:
+                levels =  make_levels(levels, neighbor, currentLevel + 1, node)
+        return levels
+
+    def make_pos(pos, node=root, currentLevel=0, parent=None, vert_loc=0):
+        dx = 1/levels[currentLevel][TOTAL]
+        left = dx/2
+        pos[node] = ((left + dx*levels[currentLevel][CURRENT])*width, vert_loc)
+        levels[currentLevel][CURRENT] += 1
+        neighbors = G.neighbors(node)
+        for neighbor in neighbors:
+            if not neighbor == parent:
+                pos = make_pos(pos, neighbor, currentLevel + 1, node, vert_loc-vert_gap)
+        return pos
+    if levels is None:
+        levels = make_levels({})
     else:
-        return np.full(len(G.edges), 0)
+        levels = {l:{TOTAL: levels[l], CURRENT:0} for l in levels}
+    vert_gap = height / (max([l for l in levels])+1)
+    return make_pos({})
 
-
-def draw_graph():
-    pos = nx.planar_layout(G)
-
-    node_labels = {n: (d["heuristics"]) for n, d in G.nodes(data=True)}
-    edge_labels = {(u, v): (d["weight"]) for u, v, d in G.edges(data=True)}
-
-    # edge_colors = draw_path()
-
-    nx.draw(G, pos, with_labels=True, node_size=200, node_color="pink", font_size=10, width=1)
-    nx.draw_networkx_edge_labels(G, pos, font_color="maroon", font_size=6)
-
-    plt.show()
-
-draw_graph()
+G=nx.Graph()
+G.add_edges_from([(1,2), (1,3), (1,4), (2,5), (2,6), (2,7), (3,8), (3,9), (4,10),
+                  (5,11), (5,12), (6,13)])
+pos = hierarchy_pos(G,1)
+nx.draw(G, pos=pos, with_labels=True)
+plt.show()
